c01beb7f26cc1ddd675753b0118174f6d6ebd5f7
GauriR971 <64442505+GauriR971@users.noreply.github.com>
2021-04-25 00:41:36 +0530

    Add files via upload

---
+++ 1madlibs.py
@@ -0,0 +1,17 @@
+# # string concatenation (aka how to put strings together) 
+# # suppose we want to create a string "subscribe to _____"
+# youtuber = "Kylie Ying" # some string variable
+
+# # a few ways to do it
+# print("subscribe to " + youtuber) 
+# print("subscribe to {}".format(youtuber)) #value of youtuber in curly brackets
+# print(f"subscribe to {youtuber}") #straightforward f-string
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb1}. \
+Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
